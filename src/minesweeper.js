184e6035cc4f915c7840852b7c0bbf7ac8a60ca3
hiram <hiram@sketchup.com>
2017-10-30 15:12:41 -0600

    newer and better

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,20 +1,58 @@
-// Print the board
-const printBoard = (board) => {
-    console.log('Current board:');
-    console.log(board[0].join(' | '));
-    console.log(board[1].join(' | '));
-    console.log(board[2].join(' | '));    
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    var newBoard = [];
+    var newRow = [];
+    for (let rows = 0; rows < numberOfRows; rows++) {
+        for (let columns = 0; columns < numberOfColumns; columns++) {
+            newRow.push(' ');          
+        }
+        newBoard.push(newRow);
+        // console.log(${newBoard}`)
+    newRow = [];
+    }
+    // console.log(newBoard);
+    return newBoard;
 }
 
-//define a blank board array
-let board = [];
-board.push([],[],[]);
-board[0].push(' ', ' ', ' ');
-board[1].push(' ', ' ', ' ');
-board[2].push(' ', ' ', ' ');
-printBoard(board);
+//Create the bomb board
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    let board = [];
+    let row = [];
+    for (let rows = 0; rows < numberOfRows; rows++) {
+        row = [];
+        for (let columns = 0; columns < numberOfColumns; columns++) {
+            row.push(null);          
+        }
+        board.push(row);
+        row.length = 0;
+    }
+    console.log(`Populating ${numberOfBombs} bombs.`);
+    var numberOfBombsPlaced = 0;
+    var randomRowIndex;
+    var randomColumnIndex;
+    while (numberOfBombsPlaced < numberOfBombs) {
+        randomRowIndex = Math.floor((Math.random() * numberOfRows));
+        randomColumnIndex = Math.floor((Math.random() * numberOfColumns));
+        board[randomRowIndex][randomColumnIndex] = "B"
+        numberOfBombsPlaced++;
+    }
+    return board;
+}
 
-//test array changes
-board[1][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+// Print the board
+ let printBoard = (board) => {
+     board.map(row =>{
+         console.log(row.join(' | '));
+     })
+
+ }
+
+let numberRows = 3;
+let numberColumns = 4;
+let numberBombs = 5;
+
+let playerBoard = generatePlayerBoard(numberRows, numberColumns);
+let bombBoard = generateBombBoard(numberRows, numberColumns, numberBombs);
+console.log('Playyer Board: ');
+printBoard(playerBoard);
+console.log('Bomb board: ');
+printBoard(bombBoard);
